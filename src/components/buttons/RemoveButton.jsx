265bc481abb85d992fa58ae385bba0a410a0146f
Ilya Kukharchuk <bartash12345@gmail.com>
2025-04-21 09:33:02 +0300

    delete btn bug fixed

--- src/components/buttons/RemoveButton.jsx
+++ src/components/buttons/RemoveButton.jsx
@@ -1,11 +1,7 @@
 export default function RemoveButton({ item, setTodos, todos, index }) {
   function removeTodo() {
-    console.log("Before: ", todos);
-    console.log("Clicked index: ", index);
-    const updatedTodos = todos.filter((_, index) => index !== index);
+    const updatedTodos = todos.filter((_, i) => i !== index);
     setTodos(updatedTodos);
-
-    console.log("After: ", updatedTodos);
   }
   return (
     <button className="btn" onClick={removeTodo}>
